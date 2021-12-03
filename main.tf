17df859baac2cbf4514d29e272345fd48aa5ecea
Rajeev Ghosh <rajeevkghosh@gmail.com>
2021-12-03 12:27:22 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "google" {
+    project = "airline1-sabre-wolverine"
+    access_token = var.access_token
+    
+}
+
+resource "google_sql_database_instance" "master" {
+  name             = "us-dev-abcd-fghi-postgres-master1"
+  database_version = "POSTGRES_11"
+  region           = "us-central1"
+  deletion_protection = false
+
+  settings {
+    # Second-generation instance tiers are based on the machine
+    # type. See argument reference below.
+    tier = "db-f1-micro"
+  }
+}
