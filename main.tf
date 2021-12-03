f1a35daa7058ed0c50ddb869cdf4576dc1976595
Rajeev Ghosh <rajeevkghosh@gmail.com>
2021-12-03 13:42:47 +0530

    7

--- main.tf
+++ main.tf
@@ -1,30 +1,31 @@
 provider "google" {
-    project = "airline1-sabre-wolverine"
-    
+  project = "airline1-sabre-wolverine"
+
 }
 
 resource "google_sql_database_instance" "master" {
-  provider = google-beta
-  name             = "us-dev-abcd-fghi-postgres-master1"
-  database_version = "POSTGRES_11"
-  region           = "us-central1"
+  provider            = google-beta
+  project             = "airline1-sabre-wolverine"
+  name                = "us-dev-abcd-fghi-postgres-master1"
+  database_version    = "POSTGRES_11"
+  region              = "us-central1"
   deletion_protection = false
   encryption_key_name = data.google_kms_crypto_key.crypto_key5.id
-  
+
 
   settings {
     # Second-generation instance tiers are based on the machine
     # type. See argument reference below.
     tier = "db-f1-micro"
-   
+
   }
 }
 
 
 data "google_kms_crypto_key" "crypto_key5" {
-  name = "us-dev-abcd-fghi-cryptokey5"
+  name     = "us-dev-abcd-fghi-cryptokey5"
   key_ring = data.google_kms_key_ring.key_ring.id
-  }
+}
 
 data "google_kms_key_ring" "key_ring" {
   name     = "us-dev-abcd-fghi-keyring3"
