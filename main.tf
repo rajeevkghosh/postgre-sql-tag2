1bb7096a211629b3e77310f60d40e4001d4b2b2e
Rajeev Ghosh <rajeevkghosh@gmail.com>
2021-12-03 12:58:55 +0530

    first commit

--- main.tf
+++ main.tf
@@ -21,5 +21,11 @@ resource "google_sql_database_instance" "master" {
 
 data "google_kms_crypto_key" "crypto_key5" {
   name = "us-dev-abcd-fghi-cryptokey5"
+  key_ring = data.google_kms_key_ring.key_ring.id
   }
+
+data "google_kms_key_ring" "key_ring" {
+  name     = "us-dev-abcd-fghi-keyring3"
+  location = "us"
+}
 
